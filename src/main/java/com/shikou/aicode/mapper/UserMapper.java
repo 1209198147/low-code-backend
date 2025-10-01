1a99baebbe3aafa2753f23224462ef1235410310
Shikou <1209198147@qq.com>
2025-10-01 19:09:23 +0800

    初始化仓库，用户模块

---
+++ src/main/java/com/shikou/aicode/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.shikou.aicode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.shikou.aicode.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
